f2037ab49b3149f97d314b316415e1d51151a8bd
agolovanov <agolovanov@eutelsat.com>
2022-09-08 10:23:55 +0300

    Внесение измений в конфигурацию CI

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM mcr.microsoft.com/playwright:v1.25.0-focal
 WORKDIR /usr/src/app
 
 RUN apt-get update && apt-get install -y openjdk-8-jdk
-RUN npm i -D playwright && npm i -D @playwright/test && npm i -D experimental-allure-playwright && npm i -D allure-commandline
+RUN npm i -D playwright && npm i -D @playwright/test && npm i -D @playwright/test allure-playwright && npm i -D allure-commandline
 
 COPY docker-entrypoint.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/docker-entrypoint.sh
--- playwright.config.ts
+++ playwright.config.ts
@@ -15,10 +15,7 @@ const config: PlaywrightTestConfig = {
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
+
     timeout: 5000
   },
   /* Run tests in files in parallel */
@@ -26,14 +23,14 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 4 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [['line'], ['allure-playwright']],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    screenshot: 'only-on-failure',
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
