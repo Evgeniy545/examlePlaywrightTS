e3488ae01f659b82d64a1174d84d1f9b32281e46
Евгений Иванов <e.ivanov>
2023-11-24 18:19:30 +0500

    fix_lint_1

--- tests/api/check_rabbit_mq.spec.ts
+++ tests/api/check_rabbit_mq.spec.ts
@@ -33,15 +33,14 @@ test.describe("Проверка очередей в ЕПГУ и в КЦ", () => 
     const deniedUpdateStatus = data5;
     await API.putReq('/v1/admin/leads/' + leadId + '/update_status', deniedUpdateStatus, token);
     const update_userJson = data_fl_user;
-    await API.putReq('/v1/admin/users/1222390', data_fl_user, token);
+    await API.putReq('/v1/admin/users/1222390', update_userJson, token);
     const inProgressUpdateStatus = data6;
     await API.putReq('/v1/admin/leads/' + leadId + '/update_status', inProgressUpdateStatus, token);
   });
 
   test('Проверка мессаджей в очереди Аттачменты статусы "Зарегистрирована", "Проверка комплектности документов"', async ({ API }) => {
     const getResRabMessage = await API.getReq('/v1/admin/rabbit_messages?messageable_type=Lead&messageable_id=' + leadId + '&queue_name=leads.attachments', token);
-    const b = (await getResRabMessage.json()).data.map((item: any) => item.attributes.message.attachments_attributes[0].kind);
-    let number: number = Number(leadId);
+    const b = (await getResRabMessage.json()).data.map((item: Record<string, any>) => item.attributes.message.attachments_attributes[0].kind);
     console.log(b);
     expect(b).toEqual(expect.arrayContaining(['consent_equity_owner', 'gas_flow', 'plan', 'title_documents', 'land', 'passport']));
   });
@@ -50,16 +49,15 @@ test.describe("Проверка очередей в ЕПГУ и в КЦ", () => 
     const getResRabMessage = await API.getReq('/v1/admin/rabbit_messages?messageable_type=Lead&messageable_id=' + leadId + '&queue_name=leads.coordinate_center', token);
     console.log((await getResRabMessage.json()));
     const b = JSON.stringify(await getResRabMessage.json());
-    expect(b).toMatch('\"Статус ЕОГ\":\"Зарегистрирована\"');
-    expect(b).toMatch('\"Статус ЕОГ\":\"Проверка комплектности документов\"');
-    expect(b).toMatch('\"Статус ЕОГ\":\"Отложена\"');
-    expect(b).toMatch('\"Статус ЕОГ\":\"Недостающие документы предоставлены\"');
+    expect(b).toMatch('"Статус ЕОГ":"Зарегистрирована"');
+    expect(b).toMatch('"Статус ЕОГ":"Проверка комплектности документов"');
+    expect(b).toMatch('"Статус ЕОГ":"Отложена"');
+    expect(b).toMatch('"Статус ЕОГ":"Недостающие документы предоставлены"');
   });
 
   test('Проверка мессаджей в очереди ЕПГУ статусы "Зарегистрирована", "Проверка комплектности документов"', async ({ API }) => {
     const getResRabMessage = await API.getReq('/v1/admin/rabbit_messages?messageable_type=Lead&messageable_id=' + leadId + '&queue_name=leads.epgu', token);
-    const b = (await getResRabMessage.json()).data.map((item: any) => item.attributes.message.status);
-    let number: number = Number(leadId);
+    const b = (await getResRabMessage.json()).data.map((item: Record<string, any>) => item.attributes.message.status);
     console.log(b);
     expect(b).toEqual(expect.arrayContaining(['registered']));
     expect(b).toEqual(expect.arrayContaining(['document_check']));
@@ -67,8 +65,7 @@ test.describe("Проверка очередей в ЕПГУ и в КЦ", () => 
 
   test('Проверка мессаджей в очереди ЕПГУ статусы "Отложена", "Недостающие документы предоставлены"', async ({ API }) => {
     const getResRabMessage = await API.getReq('/v1/admin/rabbit_messages?messageable_type=Lead&messageable_id=' + leadId + '&queue_name=leads.epgu', token);
-    const b = (await getResRabMessage.json()).data.map((item: any) => item.attributes.message.status);
-    let number: number = Number(leadId);
+    const b = (await getResRabMessage.json()).data.map((item: Record<string, any>) => item.attributes.message.status);
     console.log(b);
     expect(b).toEqual(expect.arrayContaining(['pending']));
     expect(b).toEqual(expect.arrayContaining(['documents_given']));
@@ -76,24 +73,21 @@ test.describe("Проверка очередей в ЕПГУ и в КЦ", () => 
 
   test('Проверка оргкодов 1001 1007 в мессаджах в очереди ЕПГУ для статусов "Зарегистрирована", "Проверка комплектности документов"', async ({ API }) => {
     const getResRabMessage = await API.getReq('/v1/admin/rabbit_messages?messageable_type=Lead&messageable_id=' + leadId + '&queue_name=leads.epgu', token);
-    const b = (await getResRabMessage.json()).data.map((item: any) => item.attributes.message.status+" "+item.attributes.message.org_code);
-    let number: number = Number(leadId);
+    const b = (await getResRabMessage.json()).data.map((item: Record<string, any>) => item.attributes.message.status+" "+item.attributes.message.org_code);
     console.log(b);
     expect(b).toEqual(expect.arrayContaining([ 'document_check 1007', 'registered 1001' ]));
   });
   
   test('Проверка оргкодов 1014 1007 в мессаджах в очереди ЕПГУ для статусов "Отложена", "Недостающие документы предоставлены"', async ({ API }) => {
     const getResRabMessage = await API.getReq('/v1/admin/rabbit_messages?messageable_type=Lead&messageable_id=' + leadId + '&queue_name=leads.epgu', token);
-    const b = (await getResRabMessage.json()).data.map((item: any) => item.attributes.message.status+" "+item.attributes.message.org_code);
-    let number: number = Number(leadId);
+    const b = (await getResRabMessage.json()).data.map((item: Record<string, any>) => item.attributes.message.status+" "+item.attributes.message.org_code);
     console.log(b);
     expect(b).toEqual(expect.arrayContaining([ 'pending 1014', 'documents_given 1007' ]));
   });
 
   test('Проверка оргкодов 1007 1004 в мессаджах в очереди ЕПГУ для статусов "Отказано в заключении договора", "Подготовка ТУ и заключение договора"', async ({ API }) => {
     const getResRabMessage = await API.getReq('/v1/admin/rabbit_messages?messageable_type=Lead&messageable_id=' + leadId + '&queue_name=leads.epgu', token);
-    const b = (await getResRabMessage.json()).data.map((item: any) => item.attributes.message.status+" "+item.attributes.message.org_code);
-    let number: number = Number(leadId);
+    const b = (await getResRabMessage.json()).data.map((item: Record<string, any>) => item.attributes.message.status+" "+item.attributes.message.org_code);
     console.log(b);
     expect(b).toEqual(expect.arrayContaining([ 'denied 1004', 'in_progress 1007' ]));
   });
